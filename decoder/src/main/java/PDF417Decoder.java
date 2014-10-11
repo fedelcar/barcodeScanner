8644eeb4524921d75721657e98bbd008d9b46bb2
Santiago Ramirez Ayuso <santiago.ramirez@mulesoft.com>
2014-10-11 16:54:45 -0300

    Shitty structure

---
+++ decoder/src/main/java/PDF417Decoder.java
@@ -0,0 +1,9 @@
+import com.google.zxing.pdf417.decoder.PDF417ScanningDecoder;
+
+public class PDF417Decoder
+{
+    public static void main(String[] args)
+    {
+        //PDF417ScanningDecoder.decode();
+    }
+}
